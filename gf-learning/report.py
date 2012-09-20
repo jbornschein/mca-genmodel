4497de6e7ca1dc648580fbb32a730beeadee5edd
Joerg Bornschein <bornschein@fias.uni-frankfurt.de>
2012-09-20 13:33:51 +0200

    Import actual run-em.py and report.py scripts

--- gf-learning/report.py
+++ gf-learning/report.py
@@ -2,11 +2,10 @@
 
 """
 
-Given an result-directory this script produces multiple plots:
+Given an result-directory this script produces two plots:
 
  a) time-course of the sparseness (pi*H) plottet over  EM-steps
  b) time-course of the noise-parameter (sigma) over the EM-steps
- c) ..
 
 
 """
@@ -14,78 +13,42 @@ Given an result-directory this script produces multiple plots:
 from __future__ import division
 
 import sys
-sys.path.insert(0, "../")
-
 from math import pi
 
+sys.path.insert(0, "../../../pylib")
+
+from optparse import OptionParser
+import re
 import tables
 import pylab
 import Image
 import numpy as np
 
 
+sys.path.insert(0, "../lib")
+from viz import *
+from gabor_fitting import gabor_kern
+from dog_fitting import dog_kern, find_globulars
+
 NoSuchNodeError = tables.exceptions.NoSuchNodeError
 
-
-def tiled_gfs(images, sym_cm=True, global_cm=True):
-    """
-    Return an Image (PIL) displaying all the fields stored
-    in *images*.
-
-    *images* is supposed to be of shape (N, h, w) where *N* is the number
-    of fields and (h * w) is each fields size.
-
-    *sym_cm*     Should the colormap be symmetric
-    *global_cm*  Should the final image have a global colormap, or 
-                 should the colormap be scaled on a per tile bases.
-    """
-    def vminmaxscale(arr, sym_cm):
-        vmin = arr.min()
-        vmax = arr.max()
-        if sym_cm:
-            vmax = max(-vmin, vmax)
-            vmin = -vmax
-        vscale = vmax - vmin
-        return vmin, vmax, vscale
-
-    N, h, w = images.shape
-
-    H = int(np.sqrt(N))        # W, H are no. of horizontal/vertical tiles
-    W = H
-    if H*W < N:
-        H += 1
-    if H*W < N:
-        W += 1
-
-    vmin, vmax, vscale = vminmaxscale(images, sym_cm)
-
-    img = np.zeros( (H*(h+1), W*(w+1), 4), dtype=np.uint8 )
-    for i in xrange(N):
-        tile_x = i % W
-        tile_y = i // W
-
-        x = tile_x * (w+1)
-        y = tile_y * (h+1)
-
-        if not global_cm:
-            vmin, vmax, vscale = vminmaxscale(images[i], sym_cm)
-
-        tile_img = (images[i]-vmin) / vscale
-        img[y:(y+h), x:(x+w)] = (pylab.cm.jet(tile_img)*255).astype(np.uint8)
-
-    return Image.fromarray(img[:,:,0:3])
-
+zoom = 3
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Usage: %s <directory>" % sys.argv[0]
-        exit(1)
+    parser = OptionParser(usage="Usage: %prog [options] <directory>")
+    parser.add_option("-p", "--pdf", dest="pdf", action="store_true",
+                  help="Create some PDF files")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error("icorrect number of argumenst")
 
     outdir = sys.argv[1]
     
     # Read parameter file
     params = {}
-    execfile(outdir+"paramfile.py", params)
+    execfile(outdir+"/paramfile.py", params)
 
     # Read data from result.h5
     h5 = tables.openFile(outdir+"/result.h5", "r")
@@ -104,14 +67,14 @@ if __name__ == "__main__":
     try:
         pies = h5.root.pi[:]
 
+        print "Saving sparseness plot into %s/sparseness.png..." % outdir
         pylab.plot(H*pies)
         pylab.xlabel("EM steps")
         pylab.ylabel("H*pi")
         pylab.title("Sparseness [H=%d]" % H)
         pylab.savefig(outdir+"/sparseness.png")
-        print "Saved sparseness plot into %s/sparseness.png" % outdir
     except NoSuchNodeError:
-        print "Skipped sparseness plot..."
+        print "Skipped sparseness plot"
 
 
     #=============================================================================
@@ -119,13 +82,13 @@ if __name__ == "__main__":
     try:
         sigma = h5.root.sigma[:]
 
+        print "Saving sigma plot into %s/sigma.png..." % outdir
         pylab.cla()
         pylab.plot(sigma)
         pylab.xlabel("EM steps")
         pylab.ylabel("sigma")
         pylab.title("Noise level")
         pylab.savefig(outdir+"/sigma.png")
-        print "Saved sigma plot into %s/sigma.png" % outdir
     except NoSuchNodeError:
         print "Skipped sigma plot"
 
@@ -135,47 +98,180 @@ if __name__ == "__main__":
     try:
         N_use = h5.root.N_use[:]
 
+        print "Saving N_use plot into %s/n_use.png..." % outdir
         pylab.cla()
         pylab.plot(N_use)
         pylab.xlabel("EM steps")
         pylab.ylabel("N_use")
         pylab.title("Number of datapoints used (ET cuttting)")
         pylab.savefig(outdir+"/n_use.png")
-        print "Saved N_use plot into %s/n_use.png" % outdir
     except NoSuchNodeError:
         print "Skipped N_use plot"
 
     #=============================================================================
     # nx/ny plot
     try:
-        from old_off_plot import off_plot
+        RF = h5.root.RF[0]
 
         gabor_params = h5.root.gabor_params[:]
         gabor_errors = h5.root.gabor_errors[:]
 
-        nx = gabor_params[:, 6] * gabor_params[:, 2] / 2 / pi   # XXX
-        ny = gabor_params[:, 6] * gabor_params[:, 3] / 2 / pi
-        error = -gabor_errors[:]
+        dog_params = h5.root.dog_params[:]
+        dog_errors = h5.root.dog_errors[:]
 
+        # Proper units for nx/ny plot
+        nx = np.abs(gabor_params[:, 6] * gabor_params[:, 2] / 2 / pi)   # XXX
+        ny = np.abs(gabor_params[:, 6] * gabor_params[:, 3] / 2 / pi)
+        error = gabor_errors[:]
+
+        mask = (nx < 2) * (ny < 2)
+
+        # filter DoG vs. Gabors
+        dogs = find_globulars(RF, gabor_params, gabor_errors, dog_params, dog_errors)
+        gabors = (dogs == False)
+
+        # load Ringach nx/ny 
+        #ringach_h5 = tables.openFile("../ringach-data/old-ringach-nxny.h5", "r")
+        ringach_h5 = tables.openFile("../ringach-data/self-fitted-ringach-nxny.h5", "r")
+        rnx = ringach_h5.root.R_data[0,:,0]
+        rny = ringach_h5.root.R_data[0,:,1]
+        rmask = (rnx < 1.5) * (rny < 1.5)
+        rnx = rnx[rmask]
+        rny = rny[rmask]
+        ringach_h5.close()
+
+        # Choose colormap based on filename (green vs. blue)
+        cmap = pylab.cm.Blues_r
+        if re.findall("linca", outdir):
+            cmap = pylab.cm.Greens_r;
+
+        print "Saving nxny plot into %s/nxny-all.png..." % outdir
+        print "  Error min/max: %f / %f" % (error.min(), error.max())
+        print "  nx-mean:  %4.2f" % nx[gabors*mask].mean()
+        print "  ny-mean:  %4.2f" % ny[gabors*mask].mean()
+        print "  rnx-mean: %4.2f" % rnx.mean()
+        print "  rny-mean: %4.2f" % rny.mean()
+
+        pylab.figure()
         pylab.cla()
-        pylab.scatter(nx, ny, marker='o', cmap=pylab.cm.Greys, linewidths=0.0)
+        pylab.scatter(nx[mask], ny[mask], c=error[mask], marker='o', cmap=cmap, linewidths=0.0, alpha=0.7)
+        pylab.scatter(rnx, rny, c="r", marker='^')
         diag = np.linspace(0, 1, 100)
         pylab.plot(diag, diag, color='r')
-        pylab.axis([0, 1, 0, 1])
+        pylab.axis([0.0, 1.4, 0.0, 1.4])
+        pylab.title("nx/ny plot")
+        pylab.xlabel("nx")
+        pylab.xlabel("ny")
+        pylab.savefig(outdir+"/nxny-all.png")
+        if options.pdf:
+            pylab.savefig(outdir+"/nxny-all.pdf")
+
+
+        print "Saving nxny plot into %s/nxny.png..." % outdir
+        pylab.figure()
+        pylab.cla()
+        pylab.scatter(nx[gabors*mask], ny[gabors*mask], c=error[gabors*mask], marker='o', cmap=cmap, linewidths=0.0, alpha=0.7)
+        pylab.scatter(rnx, rny, c="r", marker='^')
+        diag = np.linspace(0, 1, 100)
+        pylab.plot(diag, diag, color='r')
+        pylab.axis([0.0, 1.4, 0.0, 1.4])
         pylab.title("nx/ny plot")
         pylab.xlabel("nx")
         pylab.xlabel("ny")
         pylab.savefig(outdir+"/nxny.png")
-
-
-        pylab.figure()
-        pylab.cla()
-        off_plot(gabor_params, gabor_errors, outdir+"/nxny_old.png", discard_frac=0.01, option=3, axis=[0., 1.])
-        print "Saved old_nxny plot into %s/nxny_old.png" % outdir
-
-        print "Saved nxny plot into %s/nxny.png" % outdir
-    except (NoSuchNodeError, ImportError):
+        if options.pdf:
+            pylab.savefig(outdir+"/nxny.pdf")
+    except NoSuchNodeError:
         print "Skipped nx/ny plot!"
+        gabor_params = None
+        gabor_errors = None
+
+    #=============================================================================
+    # Fitting plot
+    try:
+        RF = h5.root.RF[0]
+
+        dog_params = h5.root.dog_params[:]
+        dog_errors = h5.root.dog_errors[:]
+
+        gabor_params = h5.root.gabor_params[:]
+        gabor_errors = h5.root.gabor_errors[:]
+
+        print "Saving fitting into %s/fitting-[dog|gabor].png..." % outdir
+
+        def save_recons(idx, fname):
+            """ Take a list of h's... save the RFs and threir reconstructions to *fname* """
+            # Construct a H*3 shaped grid of fields to be displayed
+            n_fields = len(idx)
+            shape = RF[0].shape
+        
+            if n_fields == 0:
+                return
+
+            grid = np.zeros( (n_fields, 3, shape[0], shape[1]) )
+            for i in xrange(n_fields):
+                h = idx[i]
+
+                # column 0: RF from h5 file
+                grid[i,0] = RF[h]
+
+                # column 1: Gabor reconstruction
+                gabor_recons = gabor_kern(gabor_params[h], shape)
+                grid[i,1] = gabor_recons
+
+                # column 2: DoG reconstruction
+                dog_recons = dog_kern(dog_params[h], shape)
+                grid[i,2] = dog_recons
+            grid = grid.reshape( (n_fields*3, shape[0], shape[1]) )
+
+            img = tiled_gfs(grid, sym_cm=True, global_cm=False, rows=n_fields)
+            img = img.resize( (zoom*img.size[0], zoom*img.size[1]) )
+            img.save(fname)
+
+        dogs = find_globulars(RF, gabor_params, gabor_errors, dog_params, dog_errors)
+
+        save_recons(np.where(dogs)[0], outdir+"/fitting-dog.png")
+        save_recons(np.where(dogs == False)[0], outdir+"/fitting-gabor.png")
+
+        # Save errors
+        f_gabor_error = open(outdir+"/fitting-gabor-error.txt", "w+")
+        f_dog_error = open(outdir+"/fitting-dog-error.txt", "w+")
+        for h in xrange(H):
+            if dogs[h]:
+                f_dog_error.write("%6.3f\n" % dog_errors[h])
+            else:
+                f_gabor_error.write("%6.3f\n" % gabor_errors[h])
+        f_gabor_error.close()
+        f_dog_error.close()
+    except NoSuchNodeError:
+        print "Skipped fitting-plot!"
+        gabor_params = None
+        gabor_errors = None
+
+    #=============================================================================
+    # RF plot
+    try:
+        RF = h5.root.RF[0]
+
+        print "Saving %d RFs into %s/RFs.png..." % (H, outdir)
+
+        # Reorder according to gabor matching?
+        if gabor_params is not None:
+            if gabor_params.shape[0] == H:
+                sigma_x = gabor_params[:, 2]
+                sigma_y = gabor_params[:, 3]
+                idx = np.argsort(sigma_y**2 + sigma_y**2)
+                RF = RF[idx, :]
+            else:
+                print "Number of gabor_params != H (%d != %d); NOT SORTING" % (gabor_params.shape[0], H)
+
+        # Fuse channel splitting??  (XXX subtracting  is technically wrong! XXX)
+
+        img = tiled_gfs(RF, sym_cm=True, global_cm=False)
+        img = img.resize( (zoom*img.size[0], zoom*img.size[1]) )
+        img.save(outdir+"/RF.png")
+    except NoSuchNodeError:
+        print "Skipped RF plot!"
         gabor_params = None
         gabor_errors = None
 
@@ -183,17 +279,19 @@ if __name__ == "__main__":
     # W plots
     W = h5.root.W[:]
 
-    # Reorder according to gabor matching?
-    #if gabor_params is not None:
-    #    if gabor_params.shape[0] == H:
-    #        sigma_x = gabor_params[:, 2]
-    #        sigma_y = gabor_params[:, 3]
-    #        idx = np.argsort(sigma_y)
-    #        W = W[:, idx, :]
-    #    else:
-    #        print "Number of gabor_params != H (%d != %d)" % (gabor_params.shape[0], H)
+    print "Saving %d GFs into %s/W_*.png..." % (H, outdir)
 
-    # Fuse channel splitting??  (XXX subtracting  is technically a wrong visualization! XXX)
+    # Reorder according to gabor matching?
+    if gabor_params is not None:
+        if gabor_params.shape[0] == H:
+            sigma_x = gabor_params[:, 2]
+            sigma_y = gabor_params[:, 3]
+            idx = np.argsort(sigma_y**2 + sigma_y**2)
+            W = W[:, idx, :]
+        else:
+            print "Number of gabor_params != H (%d != %d); NOT SORTING" % (gabor_params.shape[0], H)
+
+    # Fuse channel splitting??  (XXX subtracting  is technically wrong! XXX)
     if params['channel_splitted']:
         D2 = np.sqrt(D // 2)
         W = W.reshape(steps, H, 2*D2, D2)
@@ -202,18 +300,25 @@ if __name__ == "__main__":
         D2 = np.sqrt(D)
         W = W.reshape(steps, H, D2, D2)
 
-    zoom = 3
     for s in xrange(steps):
         gfs = W[s]
         img = tiled_gfs(gfs, sym_cm=True, global_cm=False)
         img = img.resize( (zoom*img.size[0], zoom*img.size[1]) )
         img.save(outdir+"/W_%03d.png" % s)
 
-    h5.close()
+    if options.pdf:
+        # Save last step as PDF
+        pylab.cla()
+        H2 = int(np.sqrt(H))+1
+        for h in xrange(H):
+            vmax=max(W[-1,h].max(), -W[-1,h].min())
+            pylab.subplot(H2, H2, h+1)
+            pylab.axis("off");
+            pylab.imshow(W[-1,h], vmin=-vmax, vmax=vmax)
+        pylab.savefig("%s/W_%03d.pdf" % (outdir, steps-1), dpi=600)
+
 
     #=============================================================================
-    # nx/ny plot
-    #if gabor_params is not None:
-    #    from gabor_fitting import off_plot 
-    # 
+    # Cleanp and go home
+    h5.close()
 
--- gf-learning/run-em.py
+++ gf-learning/run-em.py
@@ -3,10 +3,18 @@
 
 from __future__ import division
 
-# Make sure our pulp library is in the path
+#TODO: All our libraries paths are set in a source file
 import sys
-sys.path.insert(0, '../')
+sys.path.insert(0, '../lib')
+sys.path.insert(0, '../../../pylib')
 
+try:
+    import numexpr as ne
+    ne.set_num_threads(1)
+except:
+    pass
+
+import sys
 from shutil import copyfile
 
 import numpy as np
@@ -17,47 +25,46 @@ from pulp.em import EM
 from pulp.em.annealing import LinearAnnealing
 
 from pulp.utils import create_output_path
-from pulp.utils.parallel import pprint, sca_assim_load
+from pulp.utils.parallel import pprint
 from pulp.utils.datalog import dlog, StoreToH5, StoreToTxt
-from pulp.utils.tracing import set_tracefile, tracepoint
 from pulp.utils.autotable import AutoTable
+from pulp.utils.processing import deconv
+from pulp.utils import accel
 
-#from pulp.utils.processing import deconv
-#from gabor_fitting import pgabor_fit
-#from dog_fitting import pdog_fit
+import pulp.utils.tracing as tracing
+
+from gabor_fitting import pgabor_fit
+from dog_fitting import pdog_fit
 
 comm = MPI.COMM_WORLD
+
 
 #===============================================================================
 # Main
 
+paramfile = sys.argv[1]
 
+dlog.progress("%s" % ' '.join(sys.argv) )
+dlog.progress("Running %d parallel processes" % comm.size) 
+dlog.progress("Using accelerted functions: %s" % accel.backend)
+dlog.progress("Reading paramfile %s" % paramfile)
     
 # Some default parameter values
-enable_tracing = False
-anneal_prior = True
+#TODO: Agree upon default values... 
+enable_tracing = True
 partial_a = 1.  
 partial_b = 1.
 partial_c = 1.
 
-if len(sys.argv) < 2:
-    pprint();
-    pprint("Usage %s <paramfile> [<previous-output-dir-to-recover>]" % sys.argv[0])
-    pprint();
-    exit(1)
-
 # Read paramfile
-paramfile = sys.argv[1]
 execfile(paramfile)
 
 #=================== Create output path and files ==============================
 output_path = create_output_path(paramfile)
 result_path = output_path + 'result.h5'    
-if comm.rank == 0: # We keep a copy of the parameter file
+if comm.rank == 0: #We keep a copy of this file mainly because of the ET params
     copyfile(paramfile, output_path+"paramfile.py")
 
-dlog.progress("Running %d parallel processes" % comm.size) 
-dlog.progress("Reading paramfile %s" % paramfile)
 dlog.progress("Output directory: %s" % output_path)
 
 #============================= Load data =====================================
@@ -73,50 +80,96 @@ my_N = N // comm.size
 first_y = my_N * comm.rank
 last_y = my_N * (comm.rank+1)
 my_y = data_file.root.patches[first_y:last_y]
-
-# Take care of not channel splitted case
-if not channel_splitted:
-    size = data_file.root.patches.shape[2]
-    my_y = my_y[:, 0:size, :] - my_y[:, size:, :]
-
-# We flatten everything in the D dimension
-my_y = my_y.reshape(my_N, -1)
 data_file.close()
+
+if locals().has_key('rev_corr') and rev_corr:
+    data_file = openFile(rev_corr_datafile, 'r')
+    #my_y_rc = data_file.root.oversized[first_y:last_y]
+    my_y_rc = data_file.root.patches[first_y:last_y]
+    
+    orig_shape = my_y_rc.shape
+    my_y_rc = my_y_rc.reshape(my_N, -1)
+
+    # Shift into a mean free regime & normalize
+    if rev_corr_mean_free:
+        my_y_rc -= my_y_rc.mean(axis=-1)[:, None]
+    if rev_corr_normalize:
+        my_y_rc /= np.maximum( my_y_rc.max(axis=-1), -my_y_rc.min(axis=-1) )[:, None]
+        my_y_rc *= 10
+
+    my_y_rc = my_y_rc.reshape( orig_shape )
+    
+    data_file.close()
+else:
+    my_y_rc = my_y
+    rev_corr = False
+    
+
+#============================ Check format ==================================
+if my_y.shape[1] == my_y.shape[2]:
+    # New format (4 Aug 2012) here:
+    # patches are simply stored as D2 x D2 sized fields
+    if 'data_factor' in dir():
+        my_y *= data_factor
+    
+    if channel_splitted:
+        D2 = my_y.shape[1]
+        new_my_y = np.empty( (my_N,2*D2,D2))
+        new_my_y[:,:D2,:] = my_y
+        new_my_y[:,D2:,:] = -my_y
+        new_my_y[new_my_y<0.0] = 0.0
+
+        my_y = new_my_y
+        del new_my_y
+else:
+    # Old format: 
+    # patches are stored channel splitted
+    if not channel_splitted:
+        # Take care of not channel splitted case
+        size = data_file.root.patches.shape[1] / 2
+        my_y = my_y[:, 0:size, :] - my_y[:, size:, :]
+
+# We flatten everything here in the D dimension
+my_y    = my_y.reshape(my_N, -1)
+my_y_rc = my_y_rc.reshape(my_N, -1)
+
 _, D = my_y.shape
+
+
+    
 
 #========================= Configure DataLogger  ===============================
 print_list = ('T', 'pi', 'sigma', 'N', 'N_use')
-store_list = ('W', 'pi', 'sigma', 'N', 'N_use', 'Q', 'RF', 
-     'gabor_params', 'gabor_errors', 
-     'dog_params', 'dog_errors', 'dog_sigmas'
-)
+store_list = ('*')
+
 dlog.set_handler(print_list, StoreToTxt, output_path+'/terminal.txt')
 dlog.set_handler(store_list, StoreToH5, result_path)
 
 #========================= Enable tracing? =====================================
 if enable_tracing:
     dlog.progress("Tracing enabled")
-    set_tracefile(output_path+"/trace-%04d.txt")
+    tracing.set_tracefile(output_path+"/trace-%04d.txt")
 
 #=========== Create annealing schedule if not done in param file ===============
 if 'anneal' not in dir():
     anneal = LinearAnnealing(anneal_steps) 
     anneal['T'] = [(anneal_start, temp_start), (anneal_end, temp_end)] 
-    anneal['Ncut_factor'] = [(cut_start, 0.),(cut_end, 1.)] 
+    anneal['Ncut_factor'] = [(cut_start, 0.),(cut_end, 1.)]   
     anneal['anneal_prior'] = anneal_prior 
     anneal['W_noise'] = [(noise_decrease, W_noise), (noise_end, 0.)] 
     anneal['pi_noise'] = [(noise_decrease, pi_noise), (noise_end, 0.)] 
     anneal['sigma_noise'] = [(noise_decrease, sigma_noise), (noise_end, 0.)] 
     anneal['partial'] = [(cut_start, partial_a), (cut_end, partial_b), 
-                            (-11, partial_b), (-10, partial_c)] 
+                            (noise_end, partial_c)] 
 
 #========================= Instantiate model ============================== 
 model = model_class(D, H, Hprime, gamma, to_learn)
+model.rev_corr = rev_corr
 
 if len(sys.argv) == 3:
     #=============== Pick up and continue previous computation ============
     dlog.progress("Picking up computation from %s" % sys.argv[2])
-    tracepoint("Copy old reults")
+    tracing.tracepoint("Copy old reults")
 
     old_fname = sys.argv[2] + "/result.h5"
     old_h5 = openFile(old_fname, 'r')
@@ -126,6 +179,8 @@ if len(sys.argv) == 3:
         for node in old_h5.listNodes("/"):
             name = node.name
             rows = node.shape[0]
+            if name in ['RF', 'gabor_params', 'gabor_errors', 'dog_params', 'dog_errors', 'dog_sigmas']:
+                continue
             for r in xrange(rows):
                 dlog.append(name, node[r])
 
@@ -145,7 +200,7 @@ if len(sys.argv) == 3:
         anneal.next()
 else:
     #======================= Initial model parameters =======================
-    tracepoint("Estimate params")
+    tracing.tracepoint("Estimate params")
     dlog.progress("Estimating initial parameters")
     lparams = model.standard_init({'y' : my_y})
     lparams = model.noisify_params(lparams, anneal=anneal)
@@ -167,7 +222,7 @@ comm.Barrier()
 dlog.progress("Starting EM")
 
 em = EM(model=model, anneal=anneal)
-em.data =  {'y': my_y}
+em.data =  {'y': my_y, 'y_rc': my_y_rc}
 em.lparams = lparams
 em.run(verbose=True)
 lparams = em.lparams
@@ -177,45 +232,51 @@ comm.Barrier()
 
 #============================================================================
 # Extract final generative fields (W)
-GF = lparams["W"]
-H, D  = GF.shape
 
-# Undo channel splitting
-if channel_splitted:
-    size = np.int(np.sqrt(D//2))
-    GF = GF.reshape(H, 2*size, size)
-    GF = GF[:, 0:size, :] - GF[:, size:2*size, :]
+if lparams.has_key("rev_corr") and lparams['rev_corr'].max() > 1e-7:
+    dlog.progress("Using actual reverse correlated fields")
+    RF = lparams["rev_corr"]
+
+    H, D  = RF.shape
+
+   # Undo channel splitting
+    if channel_splitted:
+        size = np.int(np.sqrt(D//2))
+        RF = RF.reshape(H, 2*size, size)
+        RF = RF[:, 0:size, :] - RF[:, size:2*size, :]
+    else:
+        size = np.int(np.sqrt(D))
+        RF = RF.reshape(H, size, size)
+
 else:
-    size = np.int(np.sqrt(D))
-    GF = GF.reshape(H, size, size)
+    dlog.progress("Using W to estimate reverse fields")
+    GF = lparams["W"]
+    H, D  = GF.shape
 
-#============================================================================
-# Do postprocessing: GFs -> RFs
-tracepoint("Postprocessing")
+    # Undo channel splitting
+    if channel_splitted:
+        size = np.int(np.sqrt(D//2))
+        GF = GF.reshape(H, 2*size, size)
+        GF = GF[:, 0:size, :] - GF[:, size:2*size, :]
+    else:
+        size = np.int(np.sqrt(D))
+        GF = GF.reshape(H, size, size)
 
-if postproc == 'reverse-corr':
-    dlog.progress("Postprocessing: reverse-corr")
-    #Process with the original D
-    #the output is single channeld (H, size, size)
-    raise
-elif postproc == 'deconvolve':
-    #Really fast operation, scatering the results would take more time 
-    #than computing the same on all the processors.
+    # Do postprocessing: GFs -> RFs
     dlog.progress("Postprocessing: deconvolve")
-    RF = deconv(s_p, s_m, GF, 6*s_p)
-else:
-    dlog.progress("Postprocessing: NONE")
-    RF = GF
+    RF = deconv(s_p, s_m, GF, 10, mode='full')
 
 dlog.append('RF', RF)
 
 #=================== Gabor & DoG fit =========================================
 if fit:
     dlog.progress("Fitting gabor kernels to RFs")
-
     gabor_params, gabor_errors = pgabor_fit(RF)
     
+    old_seterr = np.seterr(all='ignore')
+    dlog.progress("Fitting DoG kernels to RFs")
     dog_params, dog_errors, dog_sigmas = pdog_fit(RF)
+    np.seterr(**old_seterr)
 
     # Store results...
     if comm.rank == 0:
@@ -229,4 +290,6 @@ if fit:
                 'dog_sigmas'    : dog_sigmas[h]
             } )
 
-tracepoint("Finished")
+#=============================================================================
+dlog.progress("Closing and archiving tracefiles")
+tracing.close()
