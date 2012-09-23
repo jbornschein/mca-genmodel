f9f15a57e4900fa231fb3eea638c32703f0eeacc
Joerg Bornschein <jb@capsec.org>
2012-09-23 12:04:34 +0200

    Download correct data

--- data/download.sh
+++ data/download.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-BASE_URL="http://fias.uni-frankfurt.de/~bornschein/data/mca-et-package"
+BASE_URL="http://fias.uni-frankfurt.de/~bornschein/NonLinSC/data/"
 
-IMAGES_FILE="vanhateren-natural.h5.bz2"
-PATCHES16_FILE="patches16.h5.bz2"
-PATCHES20_FILE="patches20.h5.bz2"
-PATCHES26_FILE="patches26.h5.bz2"
+PATCHES20_FILE="patches-20.h5"
+PATCHES26_FILE="patches-26.h5"
+PATCHES20_DOG_FILE="patches-20.h5"
+PATCHES26_DOG_FILE="patches-26.h5"
 
-IMAGES_URL="$BASE_URL/$IMAGES_FILE"
-PATCHES16_URL="$BASE_URL/$PATCHES16_FILE"
 PATCHES20_URL="$BASE_URL/$PATCHES20_FILE"
 PATCHES26_URL="$BASE_URL/$PATCHES26_FILE"
+PATCHES20_DOG_URL="$BASE_URL/$PATCHES20_DOG_FILE"
+PATCHES26_DOG_URL="$BASE_URL/$PATCHES26_DOG_FILE"
+
+DOWN_CMD="wget -c "
+
 
 echo
-echo "This script will download and unpack about 8GB training data. "
-#echo "The resulting files will occupy YYY GB of diskspace."
+echo "This script will download and unpack about 30GB training data. "
 echo 
-echo "Make sure you have wget and bunzip2 installed."
+echo "Make sure you have wget installed."
 echo 
 echo 
 echo "Press CTRL-C to abort...   (sleeping for 10 sec.)"
@@ -25,19 +27,15 @@ sleep 10
 echo -e "Allright, lets continue...\n"
 
 
-echo "\n\nDownloading Van Hateren images from $IMAGES_URL...\n\n"
-wget $IMAGES_URL
-bunzip2 $IMAGES_FILE
+echo "Downloading 20x20 patches from $PATCHES20_URL..."
+$DOWN_CMD $PATCHES20_URL
 
-echo "Downloading 16x16 pseudo-whitened patches from $PATCHES16_URL..."
-wget $PATCHES16_URL
-bunzip2 $PATCHES16_FILE
+echo "Downloading 26x26 patches from $PATCHES26_URL..."
+$DOWN_CMD $PATCHES26_URL
 
-echo "Downloading 20x20 pseudo-whitened patches from $PATCHES20_URL..."
-wget $PATCHES20_URL
-bunzip2 $PATCHES20_FILE
+echo "Downloading 20x20 DoG whitened patches from $PATCHES20_DOG_URL..."
+$DOWN_CMD $PATCHES20_URL
 
-echo "Downloading 26x26 pseudo-whitened patches from $PATCHES26_URL..."
-wget $PATCHES26_URL
-bunzip2 $PATCHES26_FILE
+echo "Downloading 26x26 DoG whitened patches from $PATCHES26_DOG_URL..."
+$DOWN_CMD $PATCHES26_DOG_URL
 
