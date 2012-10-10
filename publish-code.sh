a63e5a735046360e04dcce371eb2d8c9227a2fbe
Joerg Bornschein <bornschein@fias.uni-frankfurt.de>
2012-10-11 00:47:43 +0200

    Minor publish-code enhancement

--- publish-code.sh
+++ publish-code.sh
@@ -2,6 +2,6 @@
 
 TAG=$(date +"release-%Y-%m-%d")
 
-git tag -a $TAG -m "$TAG"
+git tag -f -a $TAG -m "$TAG"
 git archive --prefix=mca-bsc-code/ HEAD | gzip > ~/public_html/NonLinSC/mca-bsc-code.tgz
 
